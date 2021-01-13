2cfc37688a212b237f5c2600bafc52c1d14755e2
Honttinen <honttinen@gmail.com>
2021-01-13 05:08:42 +0200

    Made OrderEditor into a component

---
+++ Frontend/src/components/admin/OrderEditor.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+    <v-card :disabled="!loadedOrder">
+      <v-card-title>
+        Tilauksen {{ loadedOrder ? `#${loadedOrder.id}` : "" }} hallinta
+        <v-spacer />
+
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              icon
+              color="error"
+              @click="resetData"
+              v-bind="attrs"
+              v-on="on"
+            >
+              <v-icon>mdi-close</v-icon>
+            </v-btn>
+          </template>
+          <span>Lopeta hallinta</span>
+        </v-tooltip>
+      </v-card-title>
+      <v-divider />
+      <v-card-actions class="pa-4 justify-space-around">
+        <v-select
+          :items="statusItems"
+          item-text="tooltip"
+          item-value="id"
+          v-model="orderStatus"
+          label="Status"
+          @change="setStatus"
+          class="mr-3"
+        />
+        <DeleteOrderBtn :id="orderId" @orderDeleted="updateOrders" />
+      </v-card-actions>
+    </v-card>
+    <Orders
+      ref="OrderCard"
+      class="mt-4"
+      type="admin"
+      amount="8"
+      @orderClicked="loadOrder"
+    />
+  </div>
+</template>
+
+<script>
+import httpClient from "@/mixins/httpClient";
+
+import { mapState } from "vuex";
+
+import Orders from "@/components/Orders";
+import DeleteOrderBtn from "@/components/admin/DeleteOrderBtn";
+
+export default {
+  mixins: [httpClient],
+  components: { Orders, DeleteOrderBtn },
+
+  data: () => ({
+    loadedOrder: null,
+    orderStatus: null,
+  }),
+
+  computed: {
+    orderId() {
+      return this.loadedOrder?.id;
+    },
+    statusItems() {
+      return Object.entries(this.status).map(([key, value]) =>
+        Object.assign(value, { id: Number(key) })
+      );
+    },
+    ...mapState(["status"]),
+  },
+
+  methods: {
+    loadOrder(order) {
+      this.loadedOrder = order;
+      this.orderStatus = order.status;
+    },
+    resetData() {
+      this.loadedOrder = null;
+      this.orderStatus = null;
+    },
+    updateOrders() {
+      this.$refs.OrderCard.getOrders();
+    },
+    setStatus() {
+      this.http(`api/admin/orders/${this.orderId}`, {
+        method: "PATCH",
+        body: JSON.stringify({ status: this.orderStatus }),
+      }).then((data) => {
+        this.loadedOrder = data.data;
+        this.$refs.OrderCard.getOrders();
+      });
+    },
+  },
+};
+</script>
+
+<style></style>
--- Frontend/src/views/Admin.vue
+++ Frontend/src/views/Admin.vue
@@ -1,51 +1,33 @@
 <template>
   <v-container class="admin">
-    <v-row>
-      <v-col cols="12" sm="8" md="6" lg="4">
-        <v-card :disabled="!loadedOrder">
-          <v-card-title>
-            Tilauksen {{ loadedOrder ? `#${loadedOrder.id}` : "" }} hallinta
-            <v-spacer />
-
-            <v-tooltip bottom>
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                  icon
-                  color="error"
-                  @click="resetData"
-                  v-bind="attrs"
-                  v-on="on"
-                >
-                  <v-icon>mdi-close</v-icon>
-                </v-btn>
-              </template>
-              <span>Lopeta hallinta</span>
-            </v-tooltip>
-          </v-card-title>
-          <v-divider />
-          <v-card-actions class="pa-4 justify-space-around">
-            <v-select
-              :items="statusItems"
-              item-text="tooltip"
-              item-value="id"
-              v-model="orderStatus"
-              label="Status"
-              @change="setStatus"
-              class="mr-3"
-            />
-            <DeleteOrderBtn :id="orderId" @orderDeleted="updateOrders" />
-          </v-card-actions>
-        </v-card>
-        <Orders
-          ref="OrderCard"
-          class="mt-4"
-          type="admin"
-          @orderClicked="loadOrder"
-        />
+    <v-row class="justify-center justify-md-start">
+      <v-col cols="12" sm="10" md="6" lg="4">
+        <OrderEditor />
       </v-col>
-      <v-col cols="12" sm="8" md="6" lg="4">
+
+      <v-col cols="12" sm="10" md="6" lg="4">
         <v-card>
-          asd
+          RESTAURANT EDITOR
+        </v-card>
+
+        <v-card class="mt-4">
+          RESTAURANTS
+        </v-card>
+      </v-col>
+
+      <v-col cols="12" sm="10" md="6" lg="4">
+        <v-card>
+          USER EDITOR
+        </v-card>
+
+        <v-card class="mt-4">
+          USERS
+        </v-card>
+      </v-col>
+
+      <v-col cols="12" sm="10" md="6" lg="4">
+        <v-card>
+          PRODUCTS
         </v-card>
       </v-col>
     </v-row>
@@ -53,55 +35,9 @@
 </template>
 
 <script>
-import httpClient from "@/mixins/httpClient";
-
-import Orders from "@/components/Orders";
-import DeleteOrderBtn from "@/components/admin/DeleteOrderBtn";
-
-import { mapState } from "vuex";
+import OrderEditor from "@/components/admin/OrderEditor";
 
 export default {
-  mixins: [httpClient],
-  components: { Orders, DeleteOrderBtn },
-
-  data: () => ({
-    loadedOrder: null,
-    orderStatus: null,
-  }),
-
-  computed: {
-    orderId() {
-      return this.loadedOrder?.id;
-    },
-    statusItems() {
-      return Object.entries(this.status).map(([key, value]) =>
-        Object.assign(value, { id: Number(key) })
-      );
-    },
-    ...mapState(["status"]),
-  },
-
-  methods: {
-    loadOrder(order) {
-      this.loadedOrder = order;
-      this.orderStatus = order.status;
-    },
-    resetData() {
-      this.loadedOrder = null;
-      this.orderStatus = null;
-    },
-    updateOrders() {
-      this.$refs.OrderCard.getOrders();
-    },
-    setStatus() {
-      this.http(`api/admin/orders/${this.orderId}`, {
-        method: "PATCH",
-        body: JSON.stringify({ status: this.orderStatus }),
-      }).then((data) => {
-        this.loadedOrder = data.data;
-        this.$refs.OrderCard.getOrders();
-      });
-    },
-  },
+  components: { OrderEditor },
 };
 </script>
