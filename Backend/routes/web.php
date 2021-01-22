ff95f2085f4337b436d9d13be3397bc95786665e
Honttinen <honttinen@gmail.com>
2021-01-22 18:43:57 +0200

    Formating

--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -13,62 +13,62 @@
 
 $router->group(['prefix' => 'api'], function () use ($router) {
     $router->group(['prefix' => 'auth'], function () use ($router) {
-        $router->post('register', 'LoginController@register');  // /api/auth/register
-        $router->post('login', 'LoginController@login');        // /api/auth/login
-        $router->post('logout', 'LoginController@logout');      // /api/auth/logout
+        $router->post('register', 'LoginController@register');                  // /api/auth/register
+        $router->post('login', 'LoginController@login');                        // /api/auth/login
+        $router->post('logout', 'LoginController@logout');                      // /api/auth/logout
     });
 
     $router->group(['prefix' => 'profile'], function () use ($router) {
         $router->get('', 'UserController@getAuthenticatedUser');                // /api/profile/
         $router->patch('', 'UserController@editAuthenticatedUser');             
-        $router->patch('password', 'UserController@editAuthenticatedPassword'); // /api/profile/password
         $router->delete('', 'UserController@deleteAuthenticatedUser');          // /api/profile/
+        $router->patch('password', 'UserController@editAuthenticatedPassword'); // /api/profile/password
         $router->get('orders', 'UserController@getUserOrders');                 // /api/profile/orders
     });
 
     $router->group(['prefix' => 'stores'], function () use ($router) {
-        $router->get('', 'StoreController@getStores');      // /api/store/
+        $router->get('', 'StoreController@getStores');                          // /api/store
     });
 
     $router->group(['prefix' => 'order'], function () use ($router) {
-        $router->post('', 'OrderController@createOrder');    // /api/store/
+        $router->post('', 'OrderController@createOrder');                       // /api/order
     });
 
     $router->group(['prefix' => 'products'], function () use ($router) {
-        $router->get('', 'ProductController@getProducts');                  // /api/products/
-        $router->get('featured', 'ProductController@getFeaturedProducts');  // /api/products/featured
-        $router->get('{id}', 'ProductController@getProductById');           // /api/products/{id}
-        $router->post('list', 'ProductController@getProductsList');         // /api/products/list
+        $router->get('', 'ProductController@getProducts');                      // /api/products/
+        $router->get('featured', 'ProductController@getFeaturedProducts');      // /api/products/featured
+        $router->get('{id}', 'ProductController@getProductById');               // /api/products/{id}
+        $router->post('list', 'ProductController@getProductsList');             // /api/products/list
     });
 
     // Owner routes
     $router->group(['prefix' => 'owner', 'middleware' => ['auth', 'role:1']], function () use ($router) {
-        $router->get('orders', 'OrderController@getOrdersFiltered');        // /api/owner/orders
-        $router->get('orders/{id}', 'OrderController@getOrderById');        // /api/owner/orders/{id}
-        $router->patch('orders/{id}', 'OrderController@editOrderStatus');   // /api/owner/orders
+        $router->get('orders', 'OrderController@getOrdersFiltered');            // /api/owner/orders
+        $router->get('orders/{id}', 'OrderController@getOrderById');            // /api/owner/orders/{id}
+        $router->patch('orders/{id}', 'OrderController@editOrderStatus');       // /api/owner/orders
     });
 
     // Admin routes
     $router->group(['prefix' => 'admin', 'middleware' => ['auth', 'role:2']], function () use ($router) {
-        $router->get('users/all', 'UserController@getAllUsers');      // /api/admin/users/all
-        $router->get('users/{id}', 'UserController@getUserById');     // /api/admin/users/{id}
-        $router->delete('users/{id}', 'UserController@deleteUser');   // /api/admin/users/{id}
-        $router->post('users', 'LoginController@createUserAdmin');    // /api/admin/users/
+        $router->get('users/all', 'UserController@getAllUsers');                // /api/admin/users/all
+        $router->get('users/{id}', 'UserController@getUserById');               // /api/admin/users/{id}
+        $router->delete('users/{id}', 'UserController@deleteUser');             // /api/admin/users/{id}
+        $router->post('users', 'LoginController@createUserAdmin');              // /api/admin/users
 
-        $router->get('orders', 'OrderController@getOrders');          // /api/admin/orders
+        $router->get('orders', 'OrderController@getOrders');                    // /api/admin/orders
         $router->delete('orders/{id}', 'OrderController@deleteOrder');
         $router->patch('orders/{id}', 'OrderController@editOrderStatusAdmin');
 
-        $router->patch('products/{id}', 'ProductController@editProduct');   // /api/admin/products/{id}
+        $router->patch('products/{id}', 'ProductController@editProduct');       // /api/admin/products/{id}
         $router->delete('products/{id}', 'ProductController@deleteProduct');
 
-        $router->patch('stores/{id}', 'StoreController@editStore');     // /api/admin/stores
+        $router->patch('stores/{id}', 'StoreController@editStore');             // /api/admin/stores/{id}
         $router->delete('stores/{id}', 'StoreController@deleteStore');
 
-        $router->get('images/folders', 'ImageController@getFolders');
-        $router->get('images/{folder}', 'ImageController@getFiles');
-        $router->post('images/{folder}', 'ImageController@handleFileUpload');
-        $router->delete('images', 'ImageController@handleFileDelete');
+        $router->get('images/folders', 'ImageController@getFolders');           // /api/admin/images/folders
+        $router->get('images/{folder}', 'ImageController@getFiles');            // /api/admin/images/{folder}
+        $router->post('images/{folder}', 'ImageController@handleFileUpload');   
+        $router->delete('images', 'ImageController@handleFileDelete');          // /api/admin/images
     });
 });
 
